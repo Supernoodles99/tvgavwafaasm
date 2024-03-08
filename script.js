86854c1a175ec52194887cd0610a00c2ab3de772
Supernoodles99 <155268413+Supernoodles99@users.noreply.github.com>
2024-03-08 16:18:20 -0600

    i have no idea if this will work lol

---
+++ script.js
@@ -0,0 +1,18 @@
+let darkMode = localStorage.getItem("darkMode");
+const darkToggle = document.getElementbyId("darkModeToggle")
+if (darkMode) {
+  document.body.classList.add("darkMode");
+}
+
+function toggleDarkMode() {
+  darkMode = !darkMode;
+  if (darkMode) {
+    localStorage.setItem("darkMode", true);
+    document.body.classList.add("darkMode");
+  } else {
+    localStorage.setItem("darkMode", false);
+    document.body.classList.remove("darkMode");
+  }
+}
+
+darkToggle.addEventListener("click", toggleDarkMode());
