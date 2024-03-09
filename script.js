043a4fee7e83e668c1166b8eda8a7ddce406b22a
Supernoodles99 <155268413+Supernoodles99@users.noreply.github.com>
2024-03-08 19:20:10 -0600

    type coersion is so awesome!!!!!!!!!!!!!!1 😐

--- script.js
+++ script.js
@@ -1,16 +1,16 @@
 let darkMode = localStorage.getItem("darkMode");
 const darkToggle = document.getElementById("darkModeToggle")
-if (darkMode) {
+if (darkMode == "true") {
   document.body.classList.add("darkMode");
 }
 
 function toggleDarkMode() {
   darkMode = !darkMode;
   if (darkMode) {
-    localStorage.setItem("darkMode", true);
+    localStorage.setItem("darkMode", "true");
     document.body.classList.add("darkMode");
   } else {
-    localStorage.setItem("darkMode", false);
+    localStorage.setItem("darkMode", "false");
     document.body.classList.remove("darkMode");
   }
 }
